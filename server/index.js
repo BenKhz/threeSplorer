549ca3714c4c427c10bbc203eb5f9f88f8d7b9b8
Ben Bernardy <benbernardy@gmail.com>
2022-01-28 14:49:17 -0800

    Organizing ThreeJs Initial setup env

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
 const express = require('express')
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 const api = express();
 
 api.use(express.static('./client/dist'));
 
 api.listen(PORT, (err) => {
-  !err ? console.log(`Express server listening on ${PORT}!`) : console.log("STARTUP ERROR: ", err)
+  console.log(!err ? `Express server listening on ${PORT}!` : `ERROR: ${err}`)
 })
