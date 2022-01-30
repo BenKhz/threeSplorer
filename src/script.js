c0f544760b395cdf5734a1467063b3fc36550b51
Ben Bernardy <benbernardy@gmail.com>
2022-01-30 08:57:36 -0800

    ADDED fullscreen event listener, and resize.

--- src/script.js
+++ src/script.js
@@ -34,17 +34,39 @@ scene.add(camera)
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    alpha:true,
 })
 renderer.setSize(sizes.width, sizes.height)
+// set pixel ratio per device instance; consider including this in your tick
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-// Animate
+// Animate!!!
 const clock = new THREE.Clock()
 
 // Controls
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 
+// Update on window resizing
+window.addEventListener('resize', () => {
+    // reassign global vals to new dimensions
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+    // upate renderer sizes
+    renderer.setSize(sizes.width, sizes.height)
+    // update camera aspect ratio:
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix()
+})
+
+window.addEventListener('dblclick', ()=>{
+    if(!document.fullscreenElement) {
+        canvas.requestFullscreen()
+    } else {
+        document.exitFullscreen()
+    }
+})
 
 const tick = () =>
 {
@@ -60,5 +82,4 @@ const tick = () =>
     // Call tick again on the next frame
     window.requestAnimationFrame(tick)
 }
-
 tick()
